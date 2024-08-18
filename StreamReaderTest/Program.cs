924b5d22db3c78aa9788994a77d420e015b5d600
Bartek <bartlomiej.kacki@gmail.com>
2024-08-18 20:05:01 +0200

    Add project files.

---
+++ StreamReaderTest/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StreamReaderTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new StreamWriter("writer.txt");
+
+            for (int i = 1; i <= 10; i++)
+                writer.WriteLine("To jest linia nr {0} tekstu", i);
+
+            writer.Close();
+            writer = new StreamWriter("reader.txt");
+            var reader = new StreamReader("writer.txt");
+
+            var counter = 0;
+
+            while(!reader.EndOfStream)
+            {
+                var lineFromReader = reader.ReadLine();
+                writer.WriteLine($"Skopiowany tekst -> {lineFromReader}");
+                counter++;
+            }
+
+            /* jest to drugi sposób na zrobienie powyższej pętli while
+            var lineFromReader = reader.ReadLine();
+            while(lineFromReader != null)
+            {
+                writer.WriteLine($"Skopiowany tekst -> {lineFromReader}");
+                counter++;
+                lineFromReader = reader.ReadLine();
+            }
+            działa dokładnie tak samo */
+
+
+            Console.WriteLine(counter.ToString());
+
+            writer.Close();
+            reader.Close();
+        }
+    }
+}
